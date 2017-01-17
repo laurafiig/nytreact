cd26f9d5d5324a99392e2a76b2a48a07506d3a4d
Laura Fiig <Laura Fiig>
2017-01-17 07:23:41 -0600

    heroku database

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.use(express.static("./public"));
 // -------------------------------------------------
 
 // MongoDB Configuration configuration (Change this URL to your own DB)
-mongoose.connect("mongodb://localhost/nytimes");
-//mongoose.connect("mongodb://heroku_n81w02fc:emdrtjsilq1vu576igldh16de5@ds111559.mlab.com:11559/heroku_n81w02fc");
+//mongoose.connect("mongodb://localhost/nytimes");
+mongoose.connect("mongodb://heroku_n81w02fc:emdrtjsilq1vu576igldh16de5@ds111559.mlab.com:11559/heroku_n81w02fc");
 var db = mongoose.connection;
 
 db.on("error", function(err) {
